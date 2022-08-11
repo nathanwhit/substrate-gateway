d12012df5adf30c153b753a00d83251a19981945
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-08-11 13:04:57 -0700

    Docker publish

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -3,15 +3,7 @@
 release=$1
 tag=$2
 
-version="$(python scripts/pkg-version.py)" || exit 1
-major=$(echo "$version" | cut -d '.' -f1) || exit 1
-
-git tag -a "v${version}" -m "v${version}"
-
-docker buildx build . --platform "linux/amd64,linux/arm64" \
+docker buildx build . --platform "linux/amd64" \
     --push \
-    --label "org.opencontainers.image.url=https://github.com/subsquid/substrate-gateway/tree/$(git rev-parse HEAD)" \
-    -t "subsquid/substrate-gateway:$version" \
-    -t "subsquid/substrate-gateway:$major" \
-    -t "subsquid/substrate-gateway:$tag" \
-    -t "subsquid/substrate-gateway:$release" || exit 1
+    -t "nathanwhitaker/substrate-gateway:$tag" \
+    -t "nathanwhitaker/substrate-gateway:$release" || exit 1
